f330e663364c3af1966be105e22a040916debe1f
Rocco Hermes <rocco_hermes@genua.de>
2014-02-13 14:42:24 +0100

    Tough Day with Ruby gems
    Problem with bundler and gems installed -> Fixed

---
+++ s/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '55b69af962d7a960cf02606b7f266d8494d6a55758ec94bf39ff91c2b002ab8ede46d85ad2a5a4234be1530d3ee95ed90d53ce0c2c7f678f66f9c10a163d1dd3';
