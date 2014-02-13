55479de7ffa2fd84e14fad0fe33a63ea9a3eb1df
Rocco Hermes <rocco_hermes@genua.de>
2014-02-13 09:55:00 +0100

    Initial Commit
    Start new Projekt, RSS Feed aggregator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RsFeedAggregator::Application.config.secret_key_base = '899d910ec4a3ef23b11464a4d479e19725d6037c7c41eae32973f65f8471c6c1b2414d7aa74aa001576109afaefff22a0caf81ed94da14d5a9d26bfa0a83468b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RsFeedAggregator::Application.config.session_store :cookie_store, key: '_rs_feed_aggregator_session'
